a85899891a842a01215466ad5c8b1e595ae2751b
chenli <chenli@192.168.43.24>
2017-06-19 14:49:10 +0800

    refactor

--- SpringBoot_Swagger/src/main/java/evolution/controller/AnyController.java
+++ SpringBoot_Swagger/src/main/java/evolution/controller/AnyController.java
@@ -1,6 +1,7 @@
 package evolution.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import evolution.pojo.RequestPojo;
 import evolution.pojo.ResponsePojo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
+@Api(value = "evolution")
 public class AnyController {
 	@GetMapping("/get")
 	public ResponsePojo get(@RequestParam("name") String name) {
@@ -19,6 +23,12 @@ public class AnyController {
 	
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
 	public ResponsePojo post(@RequestBody RequestPojo request) {
+		return null;
+	}
+	
+	@PutMapping("/put")
+	@ApiOperation(value = "Put Method", response = ResponsePojo.class)
+	public ResponsePojo put() {
 		return null;
 	}
 }
